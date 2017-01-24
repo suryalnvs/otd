c5d49e2e8a22f452c5171056b318c82e98bb52c1
vagrant <vagrant@ubuntu.vagrantup.com>
2017-01-24 21:47:24 +0000

    broadcast client

--- client.go
+++ client.go
@@ -56,19 +56,38 @@ func (r *ordererdriveClient) seek(blockNumber uint64) error {
         return r.client.Send(seekHelper(r.chainID, &ab.SeekPosition{Type: &ab.SeekPosition_Specified{Specified: &ab.SeekSpecified{Number: blockNumber}}}))
 }
 
+func cleannetwork() {
+    cmd :="docker rm -f $(docker ps -aq)"
+    out, err := exec.Command("/bin/sh", "-c", cmd).Output()
+    if (err != nil) {
+      fmt.Println("Removed the Network")
+      fmt.Println(out)
+      log.Fatal(err)
+    } //else {
+      //fmt.Println("output", out)
+    //}
+}
 func launchnetwork() {
 		cmd :="docker-compose up -d"
 		out, err := exec.Command("/bin/sh", "-c", cmd).Output()
 		if (err != nil) {
 			fmt.Println("StartNetworkLocal: Could not exec docker start ")
-                        fmt.Println(err)
-			fmt.Println(out)
 			log.Fatal(err)
+      fmt.Println(out)
 		}
 }
+func network() {
+    cmd :="docker ps -a"
+    doc, err := exec.Command("/bin/sh", "-c", cmd).Output()
+    if (err != nil) {
+      fmt.Println("Unable to launch the network ", err)
+      log.Fatal(err)
+    } else {
+      fmt.Println("network is ", string(doc))
+    }
+}
 func (r *ordererdriveClient) readUntilClose() {
-        var totaltx int64
-        totaltx = 0
+        //totaltx = 0
         for {
                 msg, err := r.client.Recv()
                 if err != nil {
@@ -81,19 +100,25 @@ func (r *ordererdriveClient) readUntilClose() {
                         fmt.Println("Got status ", t)
                         return
                 case *ab.DeliverResponse_Block:
-                        totaltx += int64(len(t.Block.Data.Data))
+                        ntotal()
+                        //totaltx += int64(len(t.Block.Data.Data))
 			                  fmt.Println("Received block number: ", t.Block.Header.Number, " Transactions of the block: ", len(t.Block.Data.Data), "Total Transactions: ", totaltx)
                 }
         }
 }
-
-func main() {
+func ntotal() {
+        totaltx += int64(len(t.Block.Data.Data))
+}
+func getntotal() totaltx{
+        return totaltx
+}
+func deliverywatcher() {
         config := config.Load()
-        go launchnetwork()
+        //go launchnetwork()
         var chainID string
         var serverAddr string
 
-		    fmt.Println("After start orderers, sleep 5 secs")
+		    //fmt.Println("After start orderers, sleep 5 secs")
 		    time.Sleep(5000 * time.Millisecond)
         flag.StringVar(&serverAddr, "server", fmt.Sprintf("%s:%d", config.General.ListenAddress, config.General.ListenPort), "The RPC server to connect to.")
         flag.StringVar(&chainID, "chainID", provisional.TestChainID, "The chain ID to deliver from.")
@@ -113,7 +138,20 @@ func main() {
         s := newOrdererdriveClient(client, chainID)
         err = s.seekNewest()
         if err == nil {
-                fmt.Println("Received error:", s.seek(block.Header.Number))
+                fmt.Println("Received error:", err)
         }
         s.readUntilClose()
+}
+var totaltx int64
+func main(){
+  cleannetwork()
+  fmt.Println("==================Starting the network=========================")
+  go launchnetwork()
+  fmt.Println("After start orderers, sleep 10 secs")
+  time.Sleep(10000 * time.Millisecond)
+  fmt.Println("Printing the network")
+  network()
+  fmt.Println("===================Deliver Client=============================")
+  go deliverywatcher()
+  fmt.Println("transactions", totaltx)
 }
